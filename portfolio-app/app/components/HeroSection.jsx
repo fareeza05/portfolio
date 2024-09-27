a543c21962b57492c94887ce8ca685c85a482562
fareeza05 <fareezarahman123@gmail.com>
2024-09-27 16:36:12 -0400

    Navbar + Img changes

--- portfolio-app/app/components/HeroSection.jsx
+++ portfolio-app/app/components/HeroSection.jsx
@@ -2,52 +2,103 @@
 import React from 'react'
 import Image from 'next/image';
 import { TypeAnimation } from 'react-type-animation';
+import { FloatingDock } from './ui/floating-dock';
+import { IconHome, IconUser, IconCode, IconBriefcase2, IconBulb, IconMailSpark, IconDownload } from '@tabler/icons-react';
+import { EvervaultCard } from './ui/evervault-card';
 
+const Items = [
+    {
+        title: "Home",
+        href:"/",
+        icon: <IconHome/>
+    },
+
+    {
+        title: "About",
+        href: "/about",
+        icon: <IconUser/>
+
+    },
+
+    {
+        title: "Skills",
+        href: "/skills",
+        icon: <IconCode/>
+    },
+
+    {
+        title: "Experience",
+        href: "/exp",
+        icon: <IconBriefcase2/>
+    },
+
+    {
+        title: "Projects",
+        href:"/projects",
+        icon: <IconBulb/>
+    },
+
+    {
+        title: "Contact",
+        href:"/contact",
+        icon: <IconMailSpark/>
+    }
+
+
+]
 const HeroSection = () => {
   return (
     <section>
-        <div className='grid grid-cols-1 sm:grid-cols-12'>
+        <div className='grid grid-cols-1 sm:grid-cols-12 mt-[100px]'>
             <div className='col-span-7 place-self-center text-center sm:text-left'>
-                <h1 className='text-white mb-4  text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
-                <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-400 to-pink-400'>Hello, I'm</span>
+                <h1 className='text-white mb-4  text-4xl sm:text-5xl lg:text-6xl font-bold'>
+                <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-400 to-pink-400'>Hello, I'm Fareeza!</span>
                 <br></br>
+                </h1>
+                <p className='text-[#ADB7BE] mb-6 text-2xl sm:text-3xl lg:text-4xl'>
                 <TypeAnimation
                 sequence={[
                     // Same substring at the start will only be typed out once, initially
-                    'Fareeza',
-                    1000, // wait 1s before replacing "Mice" with "Hamsters"
-                    'Software Developer',
-                    1000,
-                    'ML Enthusiast',
+                    'Software Engineer',
                     1000,
                     'Student',
+                    1000,
+                    'ML/AI Dev',
+                    1000,
+                    'Full Stack Dev',
                     1000
                 ]}
                 wrapper="span"
                 speed={50}
                 repeat={Infinity}
                 />
-                </h1>
-                <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
-                <div>
-                    <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-400 to-pink-400 hover:bg-slate-200 text-white'>Connect With Me</button>
-                    <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-400 to-pink-400 hover:bg-slate-800 text-white mt-3'>
-                        <span className='block bg-[#121212] hover:bg-slate-100 rounded-full px-5 py-1 '>Download My Resume</span>
-                        </button>
+                </p>
+                <div className='flex items-center'>
+                    <button className='px-6 py-2.5 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-400 to-pink-400 hover:bg-slate-800 text-white'>Let's Connect!</button>
+                    <button className='flex items-center px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-400 to-pink-400 hover:bg-slate-800 text-white mt-3'>
+                    <span className='flex items-center bg-[#121212] hover:bg-slate-100 hover:text-black rounded-full px-5 py-2'>
+                        Resume
+                        <IconDownload className='w-4 h-4 ml-2' />
+                    </span>
+                    </button>
+
                 </div>
             </div>
 
             <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
-                <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative'>
-                    <Image src='/images/c765433d7a77bc87a8002b578d004e62.jpeg'
-                    alt='hero-image'
-                    className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2  left-1/3'
-                    width={150}
-                    height={150}
-                    />
+                <div className='rounded-full bg-[#181818]  w-[250px] h-[250px] lg:w-[500px] lg:h-[500px] relative mt-5'>
+                    <EvervaultCard imageSrc='/images/Retro Computer6.png'>
+                        <Image src='/images/Retro Computer6.png'
+                        height={500}
+                        width={500}
+                        />
+                    </EvervaultCard>
                 </div>
             </div>
         </div>
+        <FloatingDock items={Items}   
+        desktopClassName="fixed bottom-20 left-1/2 transform -translate-x-1/2 w-full w-[360px] mx-auto" 
+        mobileClassName="fixed bottom-4 right-4 mx-w-[300px] mx-auto" />
     </section>
   )
 }
--- portfolio-app/app/page.js
+++ portfolio-app/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <div class="container mx-auto py-4 px-12">
         <HeroSection></HeroSection>
       </div>
+      
     </main>
   );
 }
